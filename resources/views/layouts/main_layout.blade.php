878a745956cd3714cd365ae68e20ee9289873302
douglassan83 <douglassss@gmail.com>
2026-02-09 12:35:23 +0000

    Corrigir main_layout.blade.php

--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -22,7 +22,7 @@
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" defer></script>
 
-
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap%27);">
 </head>
 
 <body>
